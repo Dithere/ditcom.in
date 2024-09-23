b028faef8ac6760981e41c60b6857a17eabe1f6a
Dithere <dithere10@gmail.com>
2024-09-23 23:23:07 +0530

    Add files via upload

---
+++ pf.js
@@ -0,0 +1,8 @@
+let wa =document.querySelector("#wa")
+wa.addEventListener("click",()=>{
+    alert("WhatsApp Number is 9641897894")
+})
+let ti =document.querySelector(".ti")
+ti.addEventListener("click",()=>{
+    alert("WhatsApp Number is 9641897894")
+})
